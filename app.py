5fac4d88e04e2fa9b12176ca94644cbde704befd
Ayan Ali <ayanalizafar68@gmail.com>
2026-05-13 17:14:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ if uploaded_file:
                     try:
 
                         response = client.models.generate_content(
-                            model="gemini-2.5-flash-lite",
+                            model="gemini-3.1-flash-lite",
                             contents=prompt
                         )
 
